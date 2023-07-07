501579f9bf719ea35ef20896de8dddbc20a39a01
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2023-07-07 16:35:01 -0300

    fix: to gbq creds

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ with open('token.json', 'w') as file:
     json.dump(token_content, file)
 
 scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
+         'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/bigquery']
 credentials = Credentials.from_service_account_file('token.json', scopes=scope)
 client = gspread.authorize(credentials)
 
