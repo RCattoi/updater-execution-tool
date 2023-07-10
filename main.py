822635b0ee49ef4e7710d23d8ff7a764adc64b21
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2023-07-10 14:47:31 -0300

    feat: add all sheets

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def sendDataToBigQuery(url, worksheet, destinationTable, cancelServices=False, r
         databaseSheet['isServicesCancellationSelected'] = True
 
     if renameColumns:
-        databaseSheet = databaseSheet.rename(columns=renameColumns)
+        databaseSheet = changeColumnName(databaseSheet, renameColumns)
 
     databaseSheet = databaseSheet.drop(columns=[''])
     databaseSheet = databaseSheet.astype(str)
@@ -31,19 +31,85 @@ def sendDataToBigQuery(url, worksheet, destinationTable, cancelServices=False, r
                          project_id='execution-tool-op', if_exists='replace', credentials=credentials)
 
 
-new_names = {
-    'DATA FATAL ': 'data_fatal',
-    'PEDIDOS': 'order_id',
-    'DIAS DE ATRASO': 'dias_atraso',
-    'VALOR DA MULTA': 'valor_multa',
-    'TIPO DE MULTA': 'tipo_multa',
-    'MULTA ATUAL': 'multa_atual',
-    'VALOR DO AÉREO': 'valor_aereo',
-    'VALOR DO AÉREO - Estimado (METABASE)': 'valor_aereo',
-    'OBSERVAÇÃO': 'obs',
-    'PEDIDO ÚNICO': 'pedido_uncio',
-    'multa acumulada': 'multa_acumulada',
+def changeColumnName(destinationTable, df):
 
-}
+    if destinationTable == 'dashboards.tutelas':
+        new_names = {
+            'DATA FATAL ': 'data_fatal',
+            'PEDIDOS': 'order_id',
+            'DIAS DE ATRASO': 'dias_atraso',
+            'VALOR DA MULTA': 'valor_multa',
+            'TIPO DE MULTA': 'tipo_multa',
+            'MULTA ATUAL': 'multa_atual',
+            'VALOR DO AÉREO': 'valor_aereo',
+            'VALOR DO AÉREO - Estimado (METABASE)': 'valor_aereo',
+            'OBSERVAÇÃO': 'obs',
+            'PEDIDO ÚNICO': 'pedido_uncio',
+            'multa acumulada': 'multa_acumulada',
+        }
+    elif destinationTable == 'dashboards.flightRefund':
+        new_names = {
+            'Data ': 'date',
+            'Operador': 'operation_agent',
+            'Pedido Hurb': 'OrderIds',
+            'ID Operação': 'operation_id',
+            'Solicitou cancelamento /Devolvido': 'isCancelled',
+            'Ticket do cancelamento': 'cancellation_ticket',
+            'Tipo de reembolso': 'cancellation_type',
+            'Localizador': 'tracker',
+            'Tipo de reserva': 'reservation_type',
+            'Suplier': 'supplier',
+            'Cia Aérea': 'airline',
+            'Bilhete': 'air_ticket',
+            'Status da Solicitação': 'airline_request_status',
+            'Status OPV': 'opv_status',
+            'Valor a reembolsar / reembolsado': 'refund_value',
+            'Protocolo / BSP Link': 'bsp_link_protocol',
+            'Observações': 'obs',
+        }
+    elif destinationTable == 'dashboards.SuspensionAction':
+        new_names = {
+            'Estabelecimento': 'establishment_name',
+            'order_id':	'order_id',
+            'operation_id':	'operation_id',
+            'Reservas':	'reservation_code',
+            'Checkin':	'checkin',
+            'flight_tracker':	'flight_tracker',
+            'Embarque':	'TravelStartDateBRT',
+            'Retorno':	'ReturnDateBRT',
+            'Hotel':	'hotel',
+            'CiaAerea':	'airline',
+            'Tutela':	'guardianship',
+            'AereoCancelado':	'isAirTravelCancelled',
+            'OperadorAereo':	'AirTravelOperator',
+            'Ticket':	'ticket_number',
+            'DataTratativa':	'DealDateBRT',
+            'TerrestreCancelado':	'isTerrestrialCancelled',
+            'OperadorTerrestre':	'TerrestrialOperator',
+            'OBS':	'observation',
+            'Tipo_Bloqueio':	'BlockType',
+            'Reembolso':	'RefundValue',
+            'obsReembolso':	'RefundObservation',
+            'Multa':	'FeeValue',
+            'obsMulta': 'FeeObservation'
+        }
+    return df.rename(columns=new_names)
+
+
 sendDataToBigQuery('https://docs.google.com/spreadsheets/d/1ZI07h13_w_kNmFyPuVyVC7MksTxTZ0t4dlAib7suzFA/edit#gid=670717809',
-                   'Geral', 'dashboards.tutelas', renameColumns=new_names)
+                   'Geral', 'dashboards.tutelas', renameColumns=True)
+
+sendDataToBigQuery('https://docs.google.com/spreadsheets/d/13sEnpKNVuOjrluLu94X7i5TRLAIJE_5M2zt5N5YGH-Y/edit#gid=0',
+                   'Database', 'services_cancelations.orders')
+
+sendDataToBigQuery('https://docs.google.com/spreadsheets/d/19gFL6vA90oSCrhBZcLE0P591CwH0clvLQJ9d7xrKmqE/edit#gid=0',
+                   'Página1', 'dashboards.senacon_tutelas')
+
+sendDataToBigQuery('https://docs.google.com/spreadsheets/d/1siIH4e16AOmXmk0ZfZo_pDPefPzXcilnmSWs7BQ9tBs/edit#gid=0',
+                   'Reembolsos', 'dashboards.ReembolsosOP')
+
+sendDataToBigQuery('https://docs.google.com/spreadsheets/d/1WQxAOmBRCJX3d_6Thra-UkdTaMLdjsL7f8NBwT6yilc/edit#gid=1241935875',
+                   'Ação Reembolso_Relatório', 'dashboards.flightRefund', renameColumns=True)
+
+sendDataToBigQuery('https://docs.google.com/spreadsheets/d/1h9z1_pg3jxLh0lleoPK0Ekhp8gObd-REZOvhAns9l3I/edit#gid=1623972009',
+                   'Cancelamento Terrestre', 'dashboards.SuspensionAction', renameColumns=True)
